8d859ad4028e683c1a048f12ef6297b2f3bde155
Semeadores <semeadorese@gmail.com>
2026-06-08 14:32:34 -0400

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Service Worker for Cronograma Semeadores
-const CACHE_NAME = 'semeadores-cronograma-v7';
+const CACHE_NAME = 'semeadores-cronograma-v14';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
